f7471a523fe17588789415bd3a168b432f334e3d
Felipe França <felipefl200@gmail.com>
2022-07-25 08:39:56 -0300

    ajuste adapters

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,24 +1,19 @@
-import NextAuth from 'next-auth'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from 'next-auth/providers/google'
 import FacebookProvider from 'next-auth/providers/facebook'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+export const authOptions: NextAuthOptions = {  
+  providers: [
+    GoogleProvider({
+      clientId:<any> process.env.GOOGLE_CLIENT_ID,
+      clientSecret:<any> process.env.GOOGLE_CLIENT_SECRET
+    }),
+    FacebookProvider({
+      clientId:<any> process.env.FACEBOOK_CLIENT_ID,
+      clientSecret:<any> process.env.FACEBOOK_CLIENT_SECRET
+    })
+  ],
+  debug: process.env.NODE_ENV === 'development',
+}
 
-export default (req: NextApiRequest, res: NextApiResponse) =>
-  NextAuth(req, res, {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      }),
-      FacebookProvider({
-        clientId: process.env.FACEBOOK_CLIENT_ID,
-        clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-      })
-    ],
-    debug: process.env.NODE_ENV === 'development',
-  })
+export default NextAuth(authOptions)
